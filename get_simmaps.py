c103a421b9d66715b382129ce92a28202ae6bbd6
Ben Vaughan <vaughanb18@gmail.com>
2019-06-18 16:21:25 -0400

    arrays are not writeable for some reason

--- get_simmaps.py
+++ get_simmaps.py
@@ -2,56 +2,65 @@ import numpy as np
 from astropy.io import fits
 import random
 import scipy.io as sp
+import config
 
 #fits.open() may need to be replaced or expanded on depending on how things go
 
 
-def get_simmaps(simflag=1, verbose=1, sb=0, xc=0, clusname, nsim): # clusname, SIMFLAG=simflag. NSIM=nsim, SB=sb, XC=xc,
-#success = success, errmsg = errmsg, verbose = verbose
-    #verbose = 1 in args
-    #simflag = 1 in args
-    #sb = 0 in args
-    #xc = 0 in args
-    if simflag > 0 and len(nsim): #what is n_elements?
+def get_simmaps(clusname, nsim, simflag=1, sb=0, xc=0):
+    """
+    Purpose: I honestly have no idea what the purpose ofthis function is, i guess to get simmaps?
+    Inputs: clusname - name of the cluster
+            nsim - name of the sim number
+            simflag - 1 = do something 
+                      0 = do something else
+            sb      - 1 = do something
+                    - 0 = do something else
+            xc      - 1 = do something
+                    - 0 = do something else
+    if simflag > 0 and nsim == 0: 
         print('simmap set but nsim not supplied! Aborting.')
-    if simflag == 0:
-        nsim = np.nan
+        exit()
     cols = ['PSW', 'PMW', 'PLW']
     bigpix = [6.0,8.3,12.0]
     ncols = len(cols)
-    maps = np.empty(ncols)
+    maps = []
+    for i in range(ncols):
+        maps.append([])
     if simflag == 1:
-        for i in range(ncols-1):
-            mapfilename = clusdata + 'sim_clusters/' + clusname + '_' + cols[i] +
-            '.sav'
-            thismap = sp.readsav(mapfilename)
-            print(thismap.calcfac) #where does thismap come from?
-            thismap.calcfac = 1.0 / (thismap.calcfac * (thismap.pixsize)*
-            thismap.pixsize)
+        for i in range(ncols):
+            mapfilename = config.CLUSDATA + 'sim_clusters/' + clusname + '_' + cols[i] + '.sav'
+            thismap = sp.readsav(mapfilename, python_dict = True)
+            print(thismap['thismap'].calfac)
+            thismap['thismap'].calfac = 1.0 / (thismap['thismap'].calfac * (thismap['thismap'].pixsize)* thismap['thismap'].pixsize)
             if sb:
-                overmap = fits.open(clusdata + 'sim_clusters/' + clusname +
-                '_' + cols[i] + 'sb.fits', head) #read fits needs to be replaced
-                thismap.xclean = overmap
+                overmap = fits.open(config.CLUSDATA + 'sim_clusters/' + clusname +
+                '_' + cols[i] + '.fits') #replaced _sb.fits with .fits because there was no _sb.fits file?
+                overmap = overmap[1].data
+                thismap['thismap'].xclean[0] = overmap
                 whpl = np.where(np.isfinite(overmap) == False)
                 for index in whpl:
-                    thismap.mask[index] = 1
+                    thismap['thismap'].mask[0][index] = 1
             elif xc:
-                overmap = fits.open(clusdata + 'sim_clusters/' + clusname + '_' +
-                cols[i] + 'xc.fits', head) #read fits needs to be replaced
-                thismap.xclean = overmap
+                overmap = fits.open(config.CLUSDATA + 'sim_clusters/' + clusname +
+                '_' + cols[i] + '.fits') #replaced _xc.fits with .fits because no _xc.fits file?
+                overmap = overmap[1].data
+                thismap['thismap'].xclean[0] = overmap
                 whpl = np.where(np.isfinite(overmap) == False)
                 for index in whpl:
-                    thismap.mask[index] = 1
-            maps[i] = thismap #maybe needs PTR_NEW equivalent maybe not?
+                    thismap['thismap'].mask[0][index] = 1 #read only array ???
+                thismap['thismap'].mask = new_arr
+            maps[i] = thismap['thismap'] 
     else:
-        for i in range(n_cols-1):
-            thissim = 'nsim' #idk what format = I04 does...
-            mapfilename = clusdata + 'bethermin_sims/' + clusname +'/' + clusname +
-            '_' + cols[i] + '_sim' + thissim + '.sav'
+        for i in range(ncols):
+            thissim = str(nsim) #idk what format = I04 does...
+            mapfilename = config.CLUSDATA + 'bethermin_sims/' + clusname +'/' + clusname + '_' + cols[i] + '_sim0' + thissim + '.sav'
             thismap = sp.readsav(mapfilename)
-            mapsize = thismap.signal.shape #still don't know what thismap is.
-            noisemap = 1.0 * thismap.error * np.random.standard_normal((mapsize[0],
-            mapsize[1]))
-            thismap.signal = thismap.signal + noisemap #STILL don't know what thismap is
-            maps[i] = thismap
+            mapsize = thismap['thismap'].signal[0].shape #still don't know what thismap is.
+            noisemap = 1.0 * thismap['thismap'].error[0] * np.random.standard_normal((mapsize[0], mapsize[1]))
+            thismap['thismap'].signal[0] = thismap['thismap'].signal[0] + noisemap #STILL don't know what thismap is
+            maps[i] = thismap['thismap']
     return maps
+
+if __name__ == "__main__":
+    maps = get_simmaps('rxj1347', 100, simflag=1, xc=1)

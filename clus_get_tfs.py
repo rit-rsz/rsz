634bde4690154cc548cbca1e6bc911a2216db9bd
Ben Vaughan <vaughanb18@gmail.com>
2019-06-19 13:00:17 -0400

    some modifications

--- clus_get_tfs.py
+++ clus_get_tfs.py
@@ -2,7 +2,7 @@ import numpy as np
 import config
 
 def get_tfs(clusname):
-    tf_file = config.clus_path + clusname + '_itermap_tf.fits' #missing !CLUSSBOX don't know
+    tf_file = config.CLUSDATA + clusname + '_itermap_tf.fits' #missing !CLUSSBOX don't know
     tf_maps, err = clus_get_data(clusname,manpath=tf_file)
     if err:
         print('error when running clus_get_data: ', err)

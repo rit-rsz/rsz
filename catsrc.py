488d15a8e02431f4e89b4efc015f75ef081817ea
Ben Vaughan <vaughanb18@gmail.com>
2019-06-19 15:27:10 -0400

    actually my changes actually saved this time

--- catsrc.py
+++ catsrc.py
@@ -27,7 +27,7 @@ import scipy.io
 import numpy as np
 from config import * #(this line will give me access to all directory variables)
 import matplotlib.pyplot as plt
-import math
+import math as m
 
 import sys
 sys.path.append('utilities')
@@ -51,67 +51,237 @@ class catsrc:
     # verbose=1):
     # ,SUCCESS=success,ERRMSG=errmsg):
 
-    if not clusname:
+    if not clusname():
         print('Require a string array of cluster names as input, aborting!')
 
-    def __init__():
+    def __init__(clusname,nsim, verbose=1, cattype='24 um', savecat=0, savemap=0, saveplot=1,
+                 maketf=0, simmap=0, s2n=3,yin=0,tin=0):
         self.maps[]
         # possibly how we would get around defining these terms, not positive
-        self.verbose = 1 if not verbose else verbose
-        self.cattype = '24 um' if not cattype else cattype
-        self.savecat = 0 if not savecat else SAVECAT
-        self.savemap = 0 if not savemap else SAVEMAP
-        self.saveplot = 1 if not saveplot else saveplot
-        self.maketf = 0 if not maketf else MAKETF
-        self.sinmap = 0 if not SIMMAP else SIMMAP
-        self.s2n = 3 if not s2n else S2N
-        self.yin = 0  if not yin else YIN
-        self.tin = 0 if not tin else tin
+        self.verbose = verbose+
+        self.cattype = cattype
+        self.savecat = savecat
+        self.savemap = savemap
+        self.saveplot = saveplot
+        self.maketf = maketf
+        self.sinmap = simmap
+        self.s2n = s2n
+        self.yin = yin
+        self.tin = tin
+        self.clusname = clusname
+        self.nsim = nsim
 
+        def setup(self):
+            if self.simmap > 0 and len(self.nsim) == 0:
+                if self.verbose:
+                    print('simmap set but nsim not supplied! Aborting')
+                exit()
 
+            if self.simmap == 0:
+                self.nsim = np.nan #maybe this isn't what is being set.
 
-    def
-#   Below assign values for these variables if they are not defined
-#   I think I may have consolidated this into the def of catsrc
-#   If not we can change back to these
-        if simmap > 0 and not nsim:
-            print('simmap set but nsim not supplied!   Aborting')
+            if self.verbose:
+                print('Welcome to SZ fitter v 1.0')
 
-    #
-    # if simmap == 0:
-    #     nsim = #fill with nans??
+            if self.saveplots:
+                #call to nuplot idk what to put here instead
+                pass
 
-    print(' ')
-    if verbose:
-        print('Welcome to SZ fitter v 1.0')
-    print(' ')
+            ringw = 18.0 #arcseconds
+            calfac = ((m.pi/ 180.0) * (1.0 / 3600)*(1.0 / 3600)) * (m.pi / 4 m.log(2.0))) * 1*10**6
+            PMWthres = 5*10**-3
+            PLWthres = 8*10**-3
 
-    # if saveplots:
-        #what ever the equivalent to nuplot would be
-        # not sure if this is just setting a location or creating the file
+            ncols = 3.0
 
-    #This needs more work to be finished
-    if verbose:
-        print('Fetching cluster parameters')
-    params = get_clusparams(clusname)
+            beam = [get_spire_beam_fwhm('PSW'), get_spire_beam_fwhm('PMW'), get_spire_beam_fwhm('PLW')]
 
-    if not cgp_success:
-        # Need to make this a single string
-        errmsg = ('clus_get_clusparams exited with error' + cgp_errm)
-        if verbose:
-            print(errmsg)
+            if self.verbose:
+                print('Fetching cluster parameters')
+            params, err = clus_get_clusparams(clusname,verbose=self.verbose)
+            if err:
+                if self.verbose:
+                    print('clus_get_clusparams exited with error: ', err)
+                exit()
 
-    if verbose:
-        print('Fetching SPIRE maps')
-    if not simmap:
-        maps = get_data()
-        if not cgd_success:
-            errmsg = ('get_data exited with error' + cgd_errmsg)
-            if verbose:
-                print(errmsg)
+            if self.verbose:
+                print('Fetching SPIRE maps')
 
+            if not simmap:
+                maps, err = clus_get_data(clusname,verbose=self.verbose)
+                if err:
+                    if self.verbose:
+                        print('clus_get_data exited with error: '. err)
+                    exit()
+            else:
+                maps, err = clus_get_simmaps(clusname, simflag=self.simmap, verbose=self.verbose, nsim=self.nsim)
+                if err:
+                    if self.verbose:
+                        print('clus_get_simmaps exited with error: ', err)
+                    exit()
+                maps, err = clus_add_sziso(maps,yin=self.yin, tin=self.tin,verbose=self.verbose)
+                if err:
+                    if self.verbose:
+                        print('clus_add_sziso exited with error: ', err)
+                    exit()
 
+            ncols = len(maps)
 
+            if self.verbose:
+                print('Fetching transfer functions')
+            if not self.maketf and not self.simmap:
+                tf_maps, err = clus_get_tfs(clusname)
+                if err:
+                    maketf = 1
+                    tf_maps = clus_get_data(clusname)
+                    ncols = len(tf_maps)
+
+                    for i in range(ncols):
+                        sztm = clus_sz_template(maps[i], params, verbose=self.verbose)
+                        tf_maps[i].xclean = sztm #maybe need to change this
+                        tf_maps[i].error = np.tiles(1.0, tf_maps[i].astr.naxis)
+                        tf_maps[i].calfac = 1.0 / tf_maps[i].JY2MJy
+            if maketf:
+                if self.verbose:
+                    print('Making SZ template map.')
+                sztm, err = clus_sz_template(maps[0], params, verbose=self.verbose)
+                if err:
+                    if self.verbose:
+                        print('clus_sz_template exited with error: ', err)
+                    exit()
+
+                sztemplatemapfile = config.CLUSDATA + clusname + '_sztemplate.fits'
+                #this part creates a fits header file from data and i need to properly work out how to do this
+
+                intfmaps = np.empty(ncols)
+                for i in range(ncols):
+                    intfmaps[i] = maps[i].file[0]
+
+                conffiles = config.SMAP_PATH + 'map_making/createmap/conffiles/' + clusname + '_tf.conf' #config.SMAP_PATH needs to be filled in
+                itest = smap_transfun_pipline_v4(args) #args need to be worked out when writing this function
+
+                tf_maps, err = clus_get_tfs(clusname)
+                if err:
+                    if self.verbose:
+                        print('Cannot find created TF files, Aborting.')
+                    exit()
+
+            if self.verbose:
+                print('Fetching regression catalogs')
+            cats, err = clus_get_cats(args) #args need to be figured out
+            if err:
+                if self.verbose:
+                    print('clus_get_cats exited with error: ', err)
+                exit()
+
+            if self.verbose:
+                print('Band merging catalogs')
+            xid, err = clus_get_xid(maps, cats, savemap=self.savemap, simmap=self.simmap, verbose=self.verbose)
+            if err:
+                if self.verbose:
+                    print('clus_get_bm exited with error: ', err)
+                exit()
+
+            if self.verbose:
+                print('Regressing and subtracting catalogs')
+
+            arg, err = clus_subtract_cat(cats, maps, xid, verbose=self.verbose)
+            if err:
+                if self.verbose:
+                    print('clus_subtract_cat exited with error: ', err)
+                exit()
+
+            if self.verbose:
+                print('Generating residual source mask')
+
+            residual_mask, err = clus_residual_mask(maps,verbose=self.verbose)
+            if err:
+                if self.verbose:
+                    print('clus_residual_mask exited with error: ', err)
+                exit()
+
+            if self.verbose:
+                print('Subtracting correlated componenets')
+
+            subtracted_comps, err = clus_subtract_xcomps(maps, simflag=self.simmap, verbose=self.verbose)
+            if err:
+                if self.verbose:
+                    print('clus_subtract_xcomps exited with error: ', err)
+                exit()
+
+            if self.verbose:
+                print('Saving processed images')
+
+            err = clus_save_data(maps,yin=self.yin, tin=self.tin, simflag=self.simmap, verbose=self.verbose)
+            if err:
+                if self.verbose:
+                    print('clus_save_data exited with error: ', err)
+                exit()
+
+            if self.simmap == 0:
+                if self.verbose:
+                    print('Computing radial averages')
+
+                radave, err = clus_compute_rings(maps,params,30.0,verbose=self.verbose)
+                if err:
+                    if self.verbose:
+                        print('clus_compute_rings exited with error: ', err)
+            if self.simmap == None:  # don't see the difference between if simmap == 0 and if not simmap ??
+                tfave, err = clus_compute_rings(tf_maps, params, 30.0, verbose=self.verbose)
+                if err:
+                    if self.verbose:
+                        print('clus_compute_rings exited with error: ', err)
+                    exit()
+
+            radave[2].fluxbin[0] = np.nan #i guess this is right??
+
+            if self.verbose:
+                print('Computing beta model fit.')
+
+            if clusname == 'ms0451':
+                maxlim = 300
+            else:
+                maxlim = 450
+
+            fit, err = clus_fitsz(args) #args need to be figued out when we write this function
+            increment = fit[1,:] #don't know if this is the same as [1,*] in idl
+            print('***** Increment value in catsrc ******', increment)
+            offsets = fit[0,:]
+            if err:
+                if self.verbose:
+                    print('clus_fitsz exited with error: ', err)
+                exit()
+
+            if not self.simmap: #again not really sure if this is right.
+                if clusname == 'ms0451':
+                    maxlim = 300
+                else:
+                    maxlim = 450
+                print(maxlim)
+
+                fit, err = clus_fitsz(args) #args need to be worked out when we write the function
+                increment = fit[1,:]
+                print('***** Increment Value 2 in catsrc ******', increment)
+                offsets = fit[0,:]
+                if err:
+                    if self.verbose:
+                        print('clus_fitsz exited with error: ', err)
+                    exit()
+                increment = increment / tfamp # i don't think tfamp is defined?
+
+                err = clus_save_szfits(increment, offsets, radave, params, simflag=self.simmap, verbose=self.verbose)
+
+            else:
+                err = clus_save_szfits(increment, offsets, radave, params, simflag=self.simmap, verbose=self.verbose, outname='szout_' + str(nsim))
+            if err:
+                if self.verbose:
+                    print('clus_save_szfits exited with error: ', css_errmsg)
+                exit()
+
+            if self.saveplots:
+                #do something i don't know what UNPLOT does
+
+            return #idk if we need to return anything here lol.
 
 
 

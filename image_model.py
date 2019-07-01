a57ff72c0ec65ffbfbadd948f03dc80c9011d816
Ben Vaughan <vaughanb18@gmail.com>
2019-07-01 10:34:49 -0400

    haha

--- image_model.py
+++ image_model.py
@@ -27,21 +27,16 @@ def image_model(x,y,f,x_size,y_size, psf, data, reference_pix=0,
 
     fixed_psf = len(psf.shape)
 
-    if fixed_psf != 2:
-        space_var = psf.shape
-        #not really sure what the rest of the code is doing after this
-
-    #this is weird because we can just call ndarray.shape but whatever.
-    if fixed_psf or space_var:
-        psf_size = psf.shape
-        if not reference_pix:
-            psf_ref_pix = get_max(psf[:,:,0]) #i think this is the python equivlanet of psf[*,*,0]
+    psf_size = psf.shape
+    if not reference_pix:
+        psf_ref_pix = get_max(psf[:,:,0]) #i think this is the python equivlanet of psf[*,*,0]
 
     for i in range(nstar):
         ix = round(x[n])
         iy = round(y[n])
         if fixed_psf:
-            psf_xy = #call to image shift so need to port that.
+            psf_xy = image_shift( psf, x[i] - ix, y[i] -iy, data)
+            #call to image shift so need to port that.
 
 def get_max(array):
     s = array.shape
@@ -62,11 +57,17 @@ def get_max(array):
     p = [x[0]], y[0]]
     return p
 
+def image_shift(image, x_shift, y_shift, data, interp_type=None):
+    size = image.shape
+    extsize = size + 2
+    imag, offset = extend_array(image, extsize[0], extsize[1])
+    lo = offset
+    up = lo + size - 1
 
+    # i do not understand the returns on image_shift.
 
-
-
-
+def extend_array(arr, s0, s1, offset): #i literally don't understand what I'm reading here...
+    array = make_array(s0, s1, type=)
 
 
 def subs_to_coord(subs, n_column):

bd9c2f31f27ca83526971e2159514393f5944916
Ben Vaughan <vaughanb18@gmail.com>
2019-07-01 09:59:41 -0400

    tested xid test script and got it working, then wrote a script to interface with the XID for the RSZ pipeline, get_xid is now done just needs to be tested

--- source_handling/get_xid.py
+++ source_handling/get_xid.py
@@ -25,93 +25,128 @@ def get_xid(maps, cat, savemap=0, simmap=0, verbose=1, confusionerrors=1):
         thresh = 3.0
         mf = 1.0 #don't really know what the point of this if else was but ok.
     mJy2Jy = 1000.0 / mf
-    cols = ['PSW', 'PMW', 'PLW']
-    ncols = len(cols)
-    psfs = np.empty(ncols)
+    catdir = config.CLUSDATA # this is a placeholder for now...
+    catfile = config.CLUSDATA + 'placeholder' #this doesn't actually seem to do anything in the xid code,
+    # but you need something for this for some reason.
 
-    for i in range(ncols):
-        if cols[i] in maps[i]['band']:
-            index = i
-        if i == 0:
-            pswfits = maps[index]['file']
-        elif i == 1:
-            pmwfits = maps[index]['file']
-        elif i == 2:
-            plwfits = maps[index]['file']
-        psfs[i] = get_spire_beam_fwhm(cols[i])
+    #data from cat
+    inra = cat['ra']
+    indec = cat['dec']
 
-    xra = cat['ra']
-    xdec = cat['dec']
-    f_src = cat['flux']
+    #initializing data containers.
+    pinds = []
+    files = []
+    primary_hdus = []
+    noise_maps = []
+    data_maps = []
+    headers = []
+    pixsizes = []
+    prf_sizes = []
+    priors = []
+    prfs = []
+    for i in range(len(map)):
+        #getting data from the fits files
+        files.append(maps[i]['file'])
+        hdul = fits.open(files[i])
+        headers.append(hdul[1].header)
+        primary_hdus.append(hdul[0].header)
+        data_maps.append(hdul[1].data)
+        noise_maps.append(hdul[4].data) #don't know if this is the error image or the mask image.
+        pixsizes.append(maps[i]['pixsize'])
+        prf_sizes.append(get_spire_beam_fwhm(maps[i]['band']))
+        pinds.append(np.arange(0,101,1) * 1.0 / pixsizes[i]) #maybe this value needs to change?
 
-    if len(xra) >= 2048:
-        xra = xra[0:2048]
-        xdec = xdec[0:2048]
-        f_src = xdec[0:2048]
-  # I do not know wwhat the following code does in IDL so its hard for me to write a comparison in python
-  # IF N_ELEMENTS(xra) GT 2048 THEN BEGIN
-  #    whpl = SORT(f_src)
-  #    xra = xra[whpl[0:2048]]
-  #    xdec = xdec[whpl[0:2048]]
-  #    f_src = f_src[whpl[0:2048]]
-  # ENDIF
+        #setting up priors
+        prior = xidplus.prior(data_maps[i], noise_maps[i], primary_hdus[i], headers[i])
+        prior.prior_cat(inra, indec, catfile)
+        prior.prior_bkg(-5.0, 5)
 
-    name = str(maps[0]['name']) + '_xid'
+        #setting up prfs.
+        prf = Gaussian2DKernel(prf_sizes[i] / 2.355, x_size=101, y_size = 101) #maybe x_size and y_size need to change.
+        prf.normalize(mode='peak')
+        prfs.append(prf.array)
 
-    astromcorr = [0.0, 0.0]
+        #appending prf to prior and setting point matrix
+        prior.set_prf(prfs[i], pinds[i], pinds[i]) #prfs, xpinds, ypinds
+        prior.get_pointing_matrix()
+        prior.upper_lim_map()
 
-    if verbose:
-        print('Calling XID')
+        #appending prior to priors list.
+        priors.append(prior)\
 
-    # don't know what the below function does at all, can't find anything on internet.
-      # LSTDRV_READSMAP,pswfits,pmwfits,plwfits,xra,xdec,psfs,spirecat,$
-      #                 f_src=f_src,name=name,fcatmod=spirecat2,$
-      #                 astromcorr=astromcorr,thresh=thresh,noconf=~confusionerrors,noseg=1
-
-    if verbose:
-        print('XID done')
+    fit = SPIRE.all_bands(priors[0], priors[1], priors[2], iter=10) #number of iterations can be changed easily.
 
     xid = []
 
-    for i in range(ncols):
-        #where did spire cat come from
-        sid = spirecat2[i]['xid']
-        sra = spirecat2[i]['inra']
-        sdec= spirecat2[i]['indec']
-        pflux = spirecat2[i]['obs250'] / mJy2Jy
-        perr = spirecat2[i]['e250'] / mJy2Jy
-        sflux = spirecat2[i]['obs250'] / mJy2Jy
-        serr = spirecat2[i]['e250'] / mJy2Jy
+    xid1 = {'sid' : 1,
+            'band' : 'PSW',
+            'sra' : inra,
+            'sdec' : indec,
+            'sflux' : fit['Val_psw'],
+            'serr' : fit['sigma_psw'],
+            'pflux' : fit['Val_psw'],
+            'perr' : fit['sigma_psw'],
+            'x' : fit['Row_psw'],
+            'y' : fit['Col_psw'],
+            'model' : None,
+            'mf' : mf} #idk if perr and pflux is right there may be a conversion needed for pflux.
+            #in mikes code it has pflux = output from xid / mJy2Jy.
+    xid2 = {'sid' : 2,
+            'band' : 'PMW',
+            'sra' : inra,
+            'sdec' : indec,
+            'sflux' : fit['Val_pmw'],
+            'serr' : fit['sigma_pmw'],
+            'pflux' : fit['Val_pmw'],
+            'perr' : fit['sigma_pmw'],
+            'x' : fit['Row_pmw'],
+            'y' : fit['Col_pmw'],
+            'model' : None,
+            'mf' : mf}
 
-    #call to convert from RA/DEC to other coordinates...
+    xid3 = {'sid' : 3,
+            'band' : 'PLW',
+            'sra' : inra,
+            'sdec' : indec,
+            'sflux' : fit['Val_plw'],
+            'serr' : fit['sigma_plw'],
+            'pflux' : fit['Val_plw'],
+            'perr' : fit['sigma_plw'],
+            'x' : fit['Row_plw'],
+            'y' : fit['Col_plw'],
+            'model' : None,
+            'mf' : mf}
 
-    #can't find an equivalent for this.
-   # ;; make a fake image from the xid catalog
-   # model = IMAGE_MODEL(x,y,sflux,(*maps[icol]).astr.naxis[0],$
-   #                   (*maps[icol]).astr.naxis[1],(*maps[icol]).psf)
+    #there was another term in the dictionary sflux, pflux and sflux looked like maybe the same thing, but I'm not sure.
+    #I left it out so if there are issues with that then it is because that is gone.
+    xid.append(xid1)
+    xid.append(xid2)
+    xid.append(xid3)
+    #don't know if inra gets changed or the input is just spit back out, but in the python XID version there is
+    #Row_psw and Col_psw for all bands, I think this refers to the pixel coordinates of sources within that band.
 
-        whpl = np.where(spirecat[i]['obs250'] / mJy2Jy >= 0.0)
+    #model = image_model(x,y, sflux, maps[i]['astr']['NAXIS'][0], maps[i]['astr']['NAXIS'][1],
+    #maps[i]['psf'])
+    #need to finish converting model over to python.
 
-        xid_sb = {'id' : sid[whpl], 'band' : cols[i],
-                  'ra' : sra[whpl], 'dec' : sdec[whpl],
-                  'x' : x[whpl], 'y' : y[whpl],
-                  'flux' : sflux[whpl], 'err' : serr[whpl],
-                  'fluxi' : pflux[whpl], 'erri' : perr,
-                  'model' : model, 'mf' : mf}
 
-        xid.append(xid_sb) #i've had issues with code like this before not sure if this will work out.
+    for i in range(len(xid)):
+        whpl = []
+        for j in range(xid[i]['pflux'].shape[0]):
+            for k in range(xid[i]['pflux'].shape[1]):
+                if xid[i]['pflux'][j,k] >= 0.0:
+                    whpl.append([j,k])
+        whpl = np.array(whpl)
+
+        xid[i]['sra'] = xid[i]['sra'][whpl]
+        xid[i]['sdec'] = xid[i]['sdec'][whpl]
+        xid[i]['x'] = xid[i]['x'][whpl]
+        xid[i]['y'] = xid[i]['y'][whpl]
+        xid[i]['sflux'] = xid[i]['sflux'][whpl]
+        xid[i]['serr'] = xid[i]['serr']
+
         if savemap:
-            outfile = config.CLUSSBOX + 'clus_get_xid_model_' + cols[i] + '.fits'
-            #writefits call !
+            outfile = config.CLUSSBOX + 'clus_get_xid_model_' + maps[i]['band'] + '.fit'
+            writefits(outfile, data=model, header_dict=maps[i]['shead'])
 
-    return xid, err
-
-def sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                temp = arr[j+1]
-                arr[j+1] = arr[j]
-                arr[j] = temp
-    return arr
+    return xid

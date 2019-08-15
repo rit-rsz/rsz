ccecad58e619a7ee621e96ee08c814fe1703b470
Ben Vaughan <vaughanb18@gmail.com>
2019-08-15 13:20:39 -0400

    make_noise_map v1

--- utilities/make_noise_mask.py
+++ utilities/make_noise_mask.py
@@ -12,6 +12,14 @@
 # OUTPUTS :
 # REVISION HISTORY :
 ################################################################################
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('../source_handling')
+from get_data import get_data
+from astropy.io import fits
+from astropy.utils.data import get_pkg_data_filename
+from astropy.convolution import Gaussian2DKernel, interpolate_replace_nans
 
 def make_noise_mask(maps, col):
     """
@@ -20,14 +28,30 @@ def make_noise_mask(maps, col):
             0 = PSW, 1 = PMW, 2 = PLW
     """
 
-    for i in range(len(maps)) :
-        mapsize = maps[col]['astr'].naxis
-        errmap = map[col]['error']
-        for j in range(mapsize[0]):
-            for k in range(mapsize[1]):
-                if maps[col]['error'][j,k] == 0:
-                    errmap[j,k] = 0.005
-        #call to make a gaussian function and convol it with the map to smooth it out, but I don't think we need that?
-                    if map[j,k] >= 0.004:
-                        maps[col]['mask'][j,k] = 1
-    return maps[col]['mask']
+    mapsize = maps[col]['error'].data.shape
+    img = maps[col]['error'].data
+    #
+    # plt.imshow(img, vmin=0.002, vmax=0.005)
+    # plt.show()
+    plt.imshow(maps[col]['mask'])
+    plt.show()
+    stddev = 24 / 2.355
+    kernel = Gaussian2DKernel(x_stddev=stddev)
+    fixed_image = interpolate_replace_nans(img, kernel)
+    # plt.imshow(fixed_image)
+    # plt.show()
+    mask = np.empty(mapsize)
+    for j in range(mapsize[0]):
+        for k in range(mapsize[1]):
+            if fixed_image[j,k] == 0:
+                fixed_image[j,k] = 0.005
+    #call to make a gaussian function and convol it with the map to smooth it out, but I don't think we need that?
+            if fixed_image[j,k] >= 0.004:
+                mask[j,k] = 1
+    return mask
+
+if __name__ == '__main__':
+    maps, err = get_data('a2218')
+    mask = make_noise_mask(maps, 0)
+    plt.imshow(mask)
+    plt.show()
